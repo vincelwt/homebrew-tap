7a487e8a6adc0ce81032974c9927691340814356
gloomberb-release <actions@users.noreply.github.com>
2026-06-07 07:59:17 +0000

    Update Gloomberb to 0.9.2

--- Casks/gloomberb.rb
+++ Casks/gloomberb.rb
@@ -1,6 +1,6 @@
 cask "gloomberb" do
-  version "0.9.1"
-  sha256 "965d1e52610ebdb17d4093c3f680c7358637f21ddc8129ca846c6b8d88989547"
+  version "0.9.2"
+  sha256 "983827432e6c1da26d8d0383c321cc7b748e87e49a9b3241d16a342385f3ab2d"
 
   url "https://github.com/vincelwt/gloomberb/releases/download/v#{version}/stable-macos-arm64-Gloomberb.app.zip",
       verified: "github.com/vincelwt/gloomberb/"
