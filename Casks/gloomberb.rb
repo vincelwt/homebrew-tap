b5216f5053129585d5b8aac1f17216663f4006b0
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-09-09 23:36:19 -0400

    Remove deprecated url verification param

--- Casks/gloomberb.rb
+++ Casks/gloomberb.rb
@@ -2,8 +2,7 @@ cask "gloomberb" do
   version "0.13.3"
   sha256 "ded246e81692ef8866dce9f7c4c5805dc6b506cc8f8d7e0ac74416f03939ab14"
 
-  url "https://github.com/gloom-sh/gloomberb/releases/download/v#{version}/stable-macos-arm64-Gloomberb.app.zip",
-      verified: "github.com/gloom-sh/gloomberb/"
+  url "https://github.com/gloom-sh/gloomberb/releases/download/v#{version}/stable-macos-arm64-Gloomberb.app.zip"
   name "Gloomberb"
   desc "Open-source finance terminal"
   homepage "https://gloom.sh"
